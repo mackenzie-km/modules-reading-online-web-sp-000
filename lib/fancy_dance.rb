e8e13a63e1bb06f955bcb4ccbdaa0bdabf91673e
Mackenzie Moore <macmoore@ucdavis.edu>
2019-03-07 19:02:46 +0000

    Done.

---
+++ lib/fancy_dance.rb
@@ -0,0 +1,13 @@
+require_relative './class_methods_module.rb'
+require_relative './dance_module.rb'
+
+module FancyDance
+ module InstanceMethods
+   include Dance
+ end 
+ module ClassMethods 
+   def metadata
+     "This class produces objects that love to dance."
+   end 
+ end 
+end 
